e45d9412ee0e1444a6a0dd00643660384b59059b
GreenHouse007 <adventurebyalex@gmail.com>
2025-08-04 14:49:13 -0600

    Part 1 uplaod
    first part of the project. more to come.

---
+++ script.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
